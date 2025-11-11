ce507332d0717f63a4f70ce5115d8e9f9b5e19c9
umar.s <2377328@cmontmorency.qc.ca>
2025-11-11 01:12:32 -0500

    feat: initialize React project with Vite.

---
+++ umar-portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { createBroswerRouter } from 'react-router-dom'
+function App() {
+  const [count, setCount] = useState(0)
+  const router = createBroswerRouter([
+    
+  ])
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
